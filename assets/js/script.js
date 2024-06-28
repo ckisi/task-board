0ef6474d1021b6102092b94205e810d5ef6c0c4a
Lain <ckisielius@gmail.com>
2024-06-28 11:19:42 -0400

    added functions to save/retrieve from local storage and create task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,37 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let nextId = JSON.parse(localStorage.getItem('nextId'));
+
+function saveTasksToStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function readTasksFromStorage() {
+    let taskList = JSON.parse(localStorage.getItem('tasks'));
+
+    if (!tasks) {
+        tasks = [];
+    }
+    return tasks;
+}
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    card.crypto = randomUUID();
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    const taskCard = $('<article>')
+        .addClass('card text-dark bg-light mb-3')
+        .attr('data-task-id', task.id);
+    const taskHeader = $('<h4>').addClass('card-header h4').text(task.name);
+    const taskDescription = $('<p>').addClass('card-text').text(task.description);
+    const taskDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const taskDeleteBtn = $('<button>')
+    .addClass()
+    .text()
+    .attr()
+    //Add event listener for delete task function taskDeleteBtn.on('click', handleDeleteTask);
 }
 
 // Todo: create a function to render the task list and make cards draggable
